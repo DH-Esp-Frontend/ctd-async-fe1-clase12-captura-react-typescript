438b5f109d8710c16429d19ba7c7b300fd226867
Claudio Orlandi <claudiosorlandi@gmail.com>
2022-03-03 03:38:21 -0300

    :sparkles: implement project code with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let name: string = "Peter Steele"
+  let birthDate: string = "1962-01-01"
+  let nickName: string = "PeterTheRealGamer2008"
+  let level: number = 379
+  let coins: number = 12000
+  let isAdmin: boolean = false
+  let age;
+
+  function getAge() {
+    let currentYear: number = new Date().getFullYear()
+    let currentBirthYear: string = birthDate.split('-')[0]
+    age = currentYear - Number(currentBirthYear)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{name}</p>
+      <p>{birthDate}</p>
+      <p>{nickName}</p>
+      <p>{level}</p>
+      <p>{coins}</p>
+      <p>{isAdmin.toString()}</p>
+      <p>{age}</p>
+      <button type="button" onClick={() => getAge()}>Calcular idade</button>
     </div>
   );
 }
